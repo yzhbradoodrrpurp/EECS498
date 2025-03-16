22608c45bca8108e9428fc4baf365dfce3c75357
yzhbradoodrrpurp <zhihangyi0224@gmail.com>
2025-03-16 13:50:03 +0800

    Finish Transformer.

--- Models/Transformer.py
+++ Models/Transformer.py
@@ -5,6 +5,7 @@
 # @Software: PyCharm
 
 import torch
+import math
 
 class SelfAttention(torch.nn.Module):
     def __init__(self, input_dim, query_dim, value_dim, dtype=torch.float, device='cpu'):
@@ -48,15 +49,18 @@ class MLP(torch.nn.Module):
 
         self.model = torch.nn.Sequential(
             torch.nn.Linear(input_dim, hidden_dim),
-            torch.nn.BatchNorm1d(hidden_dim),
             torch.nn.ReLU(),
             torch.nn.Linear(hidden_dim, hidden_dim),
-            torch.nn.BatchNorm1d(hidden_dim),
             torch.nn.ReLU(),
             torch.nn.Linear(hidden_dim, output_dim)
         )
 
     def forward(self, X):
+        """
+
+        :param X: (N, T, D)
+        :return:
+        """
         scores = self.model(X)
         return scores
 
@@ -92,19 +96,67 @@ class TransformerBlock(torch.nn.Module):
 
         feature_map = self.self_attention(X) + self.residual_shortcut1(X)  # (N, T, D)
         feature_map = self.batch_norm(feature_map)  # (N, T, D)
-        scores = self.fully_connected(feature_map) + self.residual_shortcut2(feature_map)  # (N, T, D)
+        Y = self.fully_connected(feature_map) + self.residual_shortcut2(feature_map)  # (N, T, D)
 
-        return scores
+        return Y
 
-class Transformer(torch.nn.Module):
-    def __init__(self, input_dim, num_blocks, dtype=torch.float, device='cpu'):
+class WordEmbedding(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim, dtype=torch.float, device='cpu'):
+        """
+
+        :param vocab_size: size of vocabulary
+        :param embedding_dim: dimension of word vector
+        :param dtype: data type
+        :param device: device to run on
+        """
         super().__init__()
+        self.embedding = torch.nn.Embedding(vocab_size, embedding_dim, dtype=dtype, device=device)
 
-        self.transformer_blocks = [TransformerBlock(input_dim, input_dim, input_dim) for _ in range(num_blocks)]
-        self.model = torch.nn.Sequential(*self.transformer_blocks)
+    def forward(self, num):
+        word_vec = self.embedding(num)
+        return word_vec
+
+class PositionalEncoding(torch.nn.Module):
+    def __init__(self, max_seq_len, embedding_dim, dtype=torch.float, device='cpu'):
+        super().__init__()
+        self.positional_encoding = torch.nn.Embedding(max_seq_len, embedding_dim, dtype=dtype, device=device)
 
     def forward(self, X):
-        scores = self.model(X)
+        """
+
+        :param X: (N, T, D) word vector
+        :return:
+        """
+        N, T, D = X.shape
+
+        position_ids = torch.arange(T, device=X.device)  # (T,)
+        positions = self.positional_encoding(position_ids)  # (T, D)
+
+        return X + positions  # (N, T, D)
+
+class Transformer(torch.nn.Module):
+    def __init__(self, input_dim, num_blocks, max_seq_len, vocab_size=500, dtype=torch.float, device='cpu'):
+        super().__init__()
+
+        # 将 id 转换为词向量
+        self.word_embedding = WordEmbedding(vocab_size, input_dim)
+        # 给词向量进行位置编码
+        self.positional_encoding = PositionalEncoding(max_seq_len, input_dim)
+        self.transformer_blocks = [TransformerBlock(input_dim, input_dim, input_dim) for _ in range(num_blocks)]
+        self.model = torch.nn.Sequential(*self.transformer_blocks)
+        # 将结果词向量转换为在整个字典中得分的分布
+        self.linear = torch.nn.Linear(input_dim, vocab_size)
+
+    def forward(self, num):
+        """
+
+        :param num: (N, T)
+        :return:
+        """
+        X = self.word_embedding(num)  # (N, T, D)
+        X = self.positional_encoding(X)
+        Y = self.model(X)  # (N, T, D)
+        scores = self.linear(Y)  # (N, T, vocab_size)
         return scores
 
 
@@ -112,15 +164,28 @@ if __name__ == '__main__':
 
     # TODO: Positional Embedding and Word Embedding
 
-    # 生成 0～500 范围内的整数，通过 Word Encoding 转换为输入 X
-    # 通过 Positional Encoding 加上顺序
-    prompts = torch.randint(0, 500, (8, 32, 20))
-    answers = torch.randint(0, 500, (8, 32, 20))
-    N, T, D = prompts.shape
+    # NOTE: 生成 0～500 范围内的整数，有 8 组 prompt，每组 prompt 的长度为 20 个 token
+    prompts_id = torch.randint(0, 500, (8, 20))
+    answers_id = torch.randint(0, 500, (8, 20))
+    N, T = prompts_id.shape
 
-    model = Transformer(D, 4)
-    epochs = range(10)
+    # NOTE: 每个词向量的维度为 32，Transformer 模型由 4 个 Transformer Block 组成
+    # NOTE: 一组 prompt 中最多有 20 个 token，Word Embedding 中字典的大小为 500
+    model = Transformer(32, 4, 20, vocab_size=500, dtype=torch.float, device='cpu')
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-1, momentum=0.9)
+    criterion = torch.nn.CrossEntropyLoss()
+    epochs = range(20)
 
     for epoch in epochs:
-        pass
+        scores = model(prompts_id)  # (N, T, 500)
 
+        scores_flat = scores.view(N * T, -1)
+        answers_id_flat = answers_id.view(N * T,)
+
+        loss = criterion(scores_flat, answers_id_flat)
+
+        print(f'Epoch: {epoch}, Loss: {loss.item():.4f}')
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
