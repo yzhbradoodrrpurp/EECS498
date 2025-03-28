8c52c434d7d3dbfa1a858b78b45690d6c82d74b7
yzhbradoodrrpurp <zhihangyi0224@gmail.com>
2025-03-28 13:03:47 +0800

    Add more models.

--- Models/GAN.py
+++ Models/GAN.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.init as init
 
-from ResNet import ResNet
+from ResNet import ResidualBlock
 from VGG import VGG
 
 
@@ -17,7 +17,16 @@ class Generator(nn.Module):
 
     def __init__(self, channels=(3, 8, 16, 32, 64, 128, 256), num_classes=10):
         super().__init__()
-        self.generator = ResNet(channels=channels, num_classes=num_classes)
+
+        # stack up the residual blocks
+        residual_blocks = []
+        num_blocks = len(channels)
+
+        for block in range(num_blocks - 1):
+            residual_blocks.append(ResidualBlock(channels[block], channels[block + 1]))
+        residual_blocks.append(ResidualBlock(channels[num_blocks - 1], channels[0]))
+
+        self.generator = torch.nn.Sequential(*residual_blocks)
 
     def forward(self, X):
         scores = self.generator(X)
@@ -53,29 +62,35 @@ class GAN(nn.Module):
 
         if mode == 'train':
             scores = self.discriminator(X)
-            scores_generated = self.generator(noise)
+            X_generated = self.generator(noise)
+            scores_generated = self.discriminator(X_generated)
             return scores, scores_generated
         else:
-            scores_generated = self.discriminator(noise)
-            return scores_generated
+            X_generated = self.generator(noise)
+            return X_generated
 
 
 if __name__ == '__main__':
     X = torch.randint(0, 256, (64, 3, 32, 32), dtype=torch.float)
 
     model = GAN(channels=(3, 8, 16, 32, 64, 128), num_classes=2)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    optimizer_G = optim.Adam(model.generator.parameters(), lr=1e-3, betas=(0.5, 0.999))
+    optimizer_D = optim.Adam(model.discriminator.parameters(), lr=1e-3, betas=(0.5, 0.999))
     epochs = range(20)
 
     for epoch in epochs:
         scores, scores_generated = model(X)
-        loss = criterion(scores, torch.ones(64, dtype=torch.long))
-        loss += criterion(scores_generated, torch.zeros(64, dtype=torch.long))
 
-        print(f'Epoch {epoch} Loss {loss.item():.4f}')
+        generator_loss = (scores_generated[:, 0].sigmoid().log() * -1).mean()
+        discriminator_loss = (-scores[:, 1].sigmoid().log() - (1 - scores_generated[:, 0].sigmoid()).log()).mean()
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        print(f"Epoch: {epoch}, Discriminator Loss: {discriminator_loss.item():.4f}, Generator Loss: {generator_loss.item():.4f}")
 
+        optimizer_G.zero_grad()
+        optimizer_D.zero_grad()
+
+        generator_loss.backward(retain_graph=True)
+        discriminator_loss.backward()
+
+        optimizer_G.step()
+        optimizer_D.step()
