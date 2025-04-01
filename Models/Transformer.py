3bf5c1b3653082b6c2b1227b55f3add2aec68ad6
yzhbradoodrrpurp <zhihangyi0224@gmail.com>
2025-04-01 12:18:45 +0800

    Update Transformer.

--- Models/Transformer.py
+++ Models/Transformer.py
@@ -8,22 +8,27 @@ import torch
 import math
 
 class SelfAttention(torch.nn.Module):
-    def __init__(self, input_dim, query_dim, value_dim, dtype=torch.float, device='cpu'):
+    def __init__(self, input_dim, num_heads=1, dtype=torch.float, device='cpu'):
         """
         NOTE: single-head attention layer
 
         :param input_dim: dimension of input X
-        :param query_dim: dimension of query and key
-        :param value_dim: dimension of value
+        :param QKV_dim: dimension of QKV
+        :param num_heads: number of heads
         :param dtype: data type
         :param device: device to run on
         """
         super().__init__()
 
-        # query 和 key 的维度相同
-        self.query_linear = torch.nn.Linear(input_dim, query_dim, dtype=dtype, device=device)
-        self.key_linear = torch.nn.Linear(input_dim, query_dim, dtype=dtype, device=device)
-        self.value_linear = torch.nn.Linear(input_dim, value_dim, dtype=dtype, device=device)
+        self.num_heads = num_heads  # H
+        self.input_dim = input_dim  # D
+
+        assert input_dim % num_heads == 0, 'input_dim must be divisible by num_heads.'
+
+        # 将 QKV 和 input dimension 全部统一
+        self.query_linear = torch.nn.Linear(input_dim, input_dim, dtype=dtype, device=device)
+        self.key_linear = torch.nn.Linear(input_dim, input_dim, dtype=dtype, device=device)
+        self.value_linear = torch.nn.Linear(input_dim, input_dim, dtype=dtype, device=device)
 
     def forward(self, X):
         """
@@ -32,27 +37,28 @@ class SelfAttention(torch.nn.Module):
         :return:
         """
         N, T, D = X.shape
+        H = self.num_heads
+        head_dim = D // H
 
-        query = self.query_linear(X)  # (N, T, Q)
-        key = self.key_linear(X)  # (N, T, Q)
-        value = self.value_linear(X)  # (N, T, V)
+        # NOTE: 分别得到 QKV 后再拆分为多头
+        query = self.query_linear(X)  # (N, T, D)
+        query = query.view(N, T, H, D // H).permute(0, 2, 1, 3).contiguous()  # (N, H, T, head_dim)
 
-        Q = query.shape[-1]
+        key = self.key_linear(X)  # (N, T, D)
+        key = key.view(N, T, H, D // H).permute(0, 2, 1, 3).contiguous()  # (N, H, T, head_dim)
 
-        alignment_scores = query @ key.transpose(1, 2) / math.sqrt(Q)  # (N, T, T)
-        attention_weights = torch.nn.functional.softmax(alignment_scores, dim=2)  # (N, T, T)
+        value = self.value_linear(X)  # (N, T, D)
+        value = value.view(N, T, H, D // H).permute(0, 2, 1, 3).contiguous()  # (N, H, T, head_dim)
 
-        scores = attention_weights @ value  # (N, T, V)
+        alignment_scores = query @ key.transpose(2, 3) / math.sqrt(head_dim)  # (N, H, T, T)
+        attention_weights = torch.nn.functional.softmax(alignment_scores, dim=-1)  # (N, H, T, T)
+
+        scores = attention_weights @ value  # (N, H, T, head_dim)
+
+        # 将多头转换为原来的样子
+        scores = scores.permute(0, 2, 1, 3).contiguous().view(N, T, -1)
 
         return scores
-
-class MultiHeadSelfAttention(torch.nn.Module):
-
-    def __init__(self):
-        pass
-
-    def forward(self, X):
-        pass
 
 class MLP(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
@@ -61,7 +67,9 @@ class MLP(torch.nn.Module):
         self.model = torch.nn.Sequential(
             torch.nn.Linear(input_dim, hidden_dim),
             torch.nn.ReLU(),
-            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.Linear(hidden_dim, 4 * hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(4 * hidden_dim, hidden_dim),
             torch.nn.ReLU(),
             torch.nn.Linear(hidden_dim, output_dim)
         )
@@ -76,19 +84,18 @@ class MLP(torch.nn.Module):
         return scores
 
 class TransformerBlock(torch.nn.Module):
-    def __init__(self, input_dim, query_dim, value_dim, dtype=torch.float, device='cpu'):
+    def __init__(self, input_dim, num_heads=1, dtype=torch.float, device='cpu'):
         """
 
         :param input_dim: dimension of input X
-        :param query_dim: dimension of query and key
-        :param value_dim: dimension of value
+        :param QKV_dim: dimension of query, key and value
         :param dtype: data type
         :param device: device to run on
         NOTE: 一般将 query, key, value 的维度都设置为和输入的维度相同，便于进行残差连接
         """
         super().__init__()
 
-        self.self_attention = SelfAttention(input_dim, query_dim, value_dim, dtype=dtype, device=device)
+        self.self_attention = SelfAttention(input_dim, num_heads, dtype=dtype, device=device)
         # `torch.nn.Identity` 不改变输入
         self.residual_shortcut1 = torch.nn.Identity()
         # Transformer 中使用 Layer Normalization
@@ -147,18 +154,18 @@ class PositionalEncoding(torch.nn.Module):
 
         return X + positions  # (N, T, D)
 
-class Transformer(torch.nn.Module):
-    def __init__(self, input_dim, num_blocks, max_seq_len, vocab_size=500, dtype=torch.float, device='cpu'):
+class Encoder(torch.nn.Module):
+    def __init__(self, input_dim, num_heads=1, num_blocks=8, max_seq_len=32, vocab_size=500, dtype=torch.float, device='cpu'):
         super().__init__()
 
         # 将 id 转换为词向量
         self.word_embedding = WordEmbedding(vocab_size, input_dim)
         # 给词向量进行位置编码
         self.positional_encoding = PositionalEncoding(max_seq_len, input_dim)
-        self.transformer_blocks = [TransformerBlock(input_dim, input_dim, input_dim) for _ in range(num_blocks)]
+        self.transformer_blocks = [TransformerBlock(input_dim, num_heads) for _ in range(num_blocks)]
         self.model = torch.nn.Sequential(*self.transformer_blocks)
-        # 将结果词向量转换为在整个字典中得分的分布
-        self.linear = torch.nn.Linear(input_dim, vocab_size)
+        # # 将结果词向量转换为在整个字典中得分的分布
+        # self.linear = torch.nn.Linear(input_dim, vocab_size)
 
     def forward(self, num):
         """
@@ -169,8 +176,26 @@ class Transformer(torch.nn.Module):
         X = self.word_embedding(num)  # (N, T, D)
         X = self.positional_encoding(X)
         Y = self.model(X)  # (N, T, D)
-        scores = self.linear(Y)  # (N, T, vocab_size)
-        return scores
+        # scores = self.linear(Y)  # (N, T, vocab_size)
+        return Y
+
+class Decoder(torch.nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, X):
+        pass
+
+class Transformer(torch.nn.Module):
+    def __init__(self, input_dim, num_heads=1, num_blocks=8, max_seq_len=32, vocab_size=500, dtype=torch.float,
+                 device='cpu'):
+        super().__init__()
+
+        self.encoder = Encoder(input_dim, num_heads, num_blocks, max_seq_len, vocab_size, dtype, device)
+        self.decoder = Decoder()
+
+    def forward(self, X):
+        encoded_output = self.encoder(X)
 
 
 if __name__ == '__main__':
@@ -184,7 +209,7 @@ if __name__ == '__main__':
 
     # NOTE: 每个词向量的维度为 32，Transformer 模型由 4 个 Transformer Block 组成
     # NOTE: 一组 prompt 中最多有 20 个 token，Word Embedding 中字典的大小为 500
-    model = Transformer(32, 4, 20, vocab_size=500, dtype=torch.float, device='cpu')
+    model = Encoder(32, 8, 25, vocab_size=500, dtype=torch.float, device='cpu')
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-1, momentum=0.9)
     criterion = torch.nn.CrossEntropyLoss()
     epochs = range(20)
